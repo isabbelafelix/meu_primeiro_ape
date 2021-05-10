4cefc5b25bb4d68256cef00a74d3906d225843f0
Isabela_Felix <isabbelafelix@gmail.com>
2021-05-10 17:21:37 -0300

    create main screen

---
+++ app/src/main/java/com/example/projetomeuprimeiroape/Model/PrimeiraTelaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projetomeuprimeiroape.Model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projetomeuprimeiroape.R
+
+class PrimeiraTelaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primeira_tela)
+    }
+}
---
+++ app/src/main/java/com/example/projetomeuprimeiroape/Model/TelaLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projetomeuprimeiroape.Model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projetomeuprimeiroape.R
+
+class TelaLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_login)
+    }
+}
