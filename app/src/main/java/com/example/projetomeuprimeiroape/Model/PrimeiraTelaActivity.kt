707b86bec4c0bcb7d37be55b0d419aba7bebd77f
Isabela_Felix <isabbelafelix@gmail.com>
2021-05-11 22:20:21 -0300

    commit screen call login

--- app/src/main/java/com/example/projetomeuprimeiroape/Model/PrimeiraTelaActivity.kt
+++ app/src/main/java/com/example/projetomeuprimeiroape/Model/PrimeiraTelaActivity.kt
@@ -1,12 +1,35 @@
 package com.example.projetomeuprimeiroape.Model
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.projetomeuprimeiroape.R
 
 class PrimeiraTelaActivity : AppCompatActivity() {
+
+        lateinit var telaPrincipal: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primeira_tela)
+
+        carregarElementos()
+        carregarEventos()
     }
+
+
+        fun carregarElementos() {
+            telaPrincipal = findViewById<ImageView>(R.id.tela_principal)
+        }
+
+        fun carregarEventos() {
+            telaPrincipal.setOnClickListener {
+                val intent = Intent(this, TelaLoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+
 }
