7255e6e036b8e5c0c4227bf9a8dec6d56ba09ce4
Isabela_Felix <isabbelafelix@gmail.com>
2021-05-12 00:50:48 -0300

    adicionado uma chamada da tela de cadastro para tela de login

--- app/src/main/java/com/example/projetomeuprimeiroape/Model/TelaCadastroActivity.kt
+++ app/src/main/java/com/example/projetomeuprimeiroape/Model/TelaCadastroActivity.kt
@@ -1,12 +1,35 @@
 package com.example.projetomeuprimeiroape.Model
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.projetomeuprimeiroape.R
 
 class TelaCadastroActivity : AppCompatActivity() {
+
+    lateinit var telalogin: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_cadastro)
+
+        carregarElementos()
+        carregarEventos()
     }
+
+    fun carregarElementos(){
+        telalogin = findViewById<TextView>(R.id.text_entrar)
+    }
+
+    fun carregarEventos(){
+        telalogin.setOnClickListener {
+            val intent = Intent(this, TelaLoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
 }
+
