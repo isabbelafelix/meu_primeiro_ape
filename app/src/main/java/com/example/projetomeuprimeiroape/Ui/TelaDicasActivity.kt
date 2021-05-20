534c045eedd47130b39af8e85e28f271e8f81fff
Isabela_Felix <isabbelafelix@gmail.com>
2021-05-20 09:38:31 -0300

    coloquei evento de clique da tela de dicas para minha lista!!!!! :)

--- app/src/main/java/com/example/projetomeuprimeiroape/Ui/TelaDicasActivity.kt
+++ app/src/main/java/com/example/projetomeuprimeiroape/Ui/TelaDicasActivity.kt
@@ -1,18 +1,41 @@
 package com.example.projetomeuprimeiroape.Ui
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.projetomeuprimeiroape.R
+import com.example.projetomeuprimeiroape.Ui.Fragments.FragmentsMainActivity
 
 class TelaDicasActivity : AppCompatActivity() {
+
+    lateinit var telaAluguel: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_dicas)
 
-        val email = intent.getStringExtra("intent_email")
+//        val email = intent.getStringExtra("intent_email")
+//
+//        val sharedPrefs = getSharedPreferences("cadastro $email", Context.MODE_PRIVATE)
 
-        val sharedPrefs = getSharedPreferences("cadastro $email", Context.MODE_PRIVATE)
+        carregarElementos()
+        carregarEventos()
+
+    }
+
+    fun carregarElementos(){
+        telaAluguel = findViewById(R.id.buttonAluguel)
+
+    }
+
+    fun carregarEventos() {
+        telaAluguel.setOnClickListener {
+            val intent = Intent(this, FragmentsMainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
