91e55289d867a2bff59ad0ae38768895605d5c21
Isabela_Felix <isabbelafelix@gmail.com>
2021-05-20 01:29:37 -0300

    criação da classe de listas do fragmento

---
+++ app/src/main/java/com/example/projetomeuprimeiroape/Ui/Fragments/CaracteristicasListasFragment.kt
@@ -0,0 +1,13 @@
+package com.example.projetomeuprimeiroape.Ui.Fragments
+
+import androidx.fragment.app.Fragment
+
+class CaracteristicasListasFragment : Fragment(){
+
+    companion object {
+        fun newInstance() = CaracteristicasListasFragment()
+    }
+
+
+
+}
